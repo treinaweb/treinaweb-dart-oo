73666ff0c1911ed9df2142b34073288ee3ac6d89
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-07-13 10:27:21 -0300

    03.06_Instanciando_Objetos

--- main.dart
+++ main.dart
@@ -1,1 +1,31 @@
+import 'dart:io';
 
+import 'pessoa.dart';
+
+main() {
+  var opcao;
+  do {
+    print("MENU: ");
+    print("1 - Cadastrar Cliente");
+    print("2 - Listar Clientes");
+    print("0 - Sair");
+    opcao = int.parse(stdin.readLineSync());
+    switch (opcao) {
+      case 1:
+        var cliente = new Pessoa();
+        print("Digite o nome do cliente: ");
+        cliente.nome = stdin.readLineSync();
+        print("Digite a idade do cliente: ");
+        cliente.idade = int.parse(stdin.readLineSync());
+        print("Digite o email do cliente: ");
+        cliente.email = stdin.readLineSync();
+        print("Digite o sexo do cliente: ");
+        cliente.sexo = stdin.readLineSync();
+        print(cliente.nome);
+        break;
+      case 2:
+        break;
+      default:
+    }
+  } while (opcao != 0);
+}
